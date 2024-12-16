cbfc943016b8e7a8548c4daaf576779877951ca0
Nicola <bianco.nicola@yahoo.it>
2024-12-16 10:24:20 +0100

    shows minimum and maximum temperature

--- src/main/java/com/arduino/repository/ArduinoRepository.java
+++ src/main/java/com/arduino/repository/ArduinoRepository.java
@@ -2,8 +2,15 @@ package com.arduino.repository;
 
 import com.arduino.entity.ArduinoData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArduinoRepository extends JpaRepository<ArduinoData, Long> {
+
+    @Query("SELECT MIN(t.temperature) FROM ArduinoData t")
+    String findTemperaturaMinima();
+
+    @Query("SELECT MAX(t.temperature) FROM ArduinoData t")
+    String findTemperaturaMassima();
 }
