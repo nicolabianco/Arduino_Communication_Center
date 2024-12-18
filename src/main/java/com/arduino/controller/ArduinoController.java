b5aa4b3851f78c4a19fc94aa5e8cca573ad89c7c
Nicola <bianco.nicola@yahoo.it>
2024-12-18 11:37:25 +0100

    add openApi, manage controller response

--- src/main/java/com/arduino/controller/ArduinoController.java
+++ src/main/java/com/arduino/controller/ArduinoController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/data")
@@ -20,6 +17,7 @@ public class ArduinoController {
 
 
     @PostMapping("/receiveData")
+    @ResponseBody
     public ResponseEntity<String> receiveData(@RequestBody ArduinoDTO dto) {
         arduService.manageData(dto);
         return ResponseEntity.ok("dati ricevuti e elaborati");
